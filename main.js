41de6d4e28022df7f0ba54d882ed46afbd27823c
Shubham Patlani <Spatlani@gmail.com>
2023-09-27 21:15:55 +0530

    Header glitch logo added

--- main.js
+++ main.js
@@ -55,8 +55,9 @@ spans = [...document.querySelectorAll('.hero-container span')];
 function revealSpans() {
     for (let i = 0; i < spans.length; i++) {
         let { left, top } = spans[i].getBoundingClientRect();
-        top = top - (window.innerHeight * 0.35);
-        const calc = ((top * 0.1) + (left * 0.001))
+        top = top - (window.innerHeight * 0.25);
+        const calc = ((top * 0.01) + (left * 0.001))
+        console.log(calc)
         let opacityValue = 1 - calc < 0.1 ? 0.1 : 1 - calc.toFixed(3);
         opacityValue = opacityValue > 1 ? 1 : opacityValue.toFixed(3);
         spans[i].style.opacity = opacityValue
@@ -64,6 +65,9 @@ function revealSpans() {
 }
 
 const projects = [];
+
+function createMenu() {
+}
 
 let scrollable = document.querySelector('.scrollable');
 
@@ -279,4 +283,5 @@ class MeshItem{
 }
 displayWraps()
 init()
+createMenu()
 new EffectCanvas()
